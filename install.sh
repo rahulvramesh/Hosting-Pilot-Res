c7c06a1a2d480250ef39a8014c8d32838ea5cb76
Rahul v Ramesh <rahulvramesh@hotmail.com>
2015-06-06 12:16:26 +0530

    Second

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 # Script Version : Beta 0.0.1 
 # Author : Rahul V Ramesh
 
+# Check For Panels
+if [ -e /usr/local/cpanel ] || [ -e /usr/local/directadmin ] || [ -e /usr/local/solusvm/www ] || [ -e /usr/local/home/admispconfig ] || [ -e /usr/local/lxlabs/kloxo ] ; then
+    echo "Please re-install your OS before attempting to install using this script."
+    exit
+fi
 
 # Check For Installs Devcot , Apache2 , Mysql Etc ...
 if dpkg -s php apache mysql bind postfix dovecot; then
